195e3edac666c9cff2de95471f444d6ab0863c6f
Vlad <tanasievladcristian@gmail.com>
2021-01-03 00:06:11 +0200

    added todo component

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: "my first todo",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "my second todo",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "my third todo",
+        completed: false
+      }
+    ]
   }
 
 }
